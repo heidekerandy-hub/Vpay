385fe3ee4cff4a3606467432b99d6546c281d58b
heidekerandy-hub <heidekerandy@gmail.com>
2026-08-27 11:07:08 +0100

    Add VPay JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("VPay app loaded successfully");
+
+const addMoneyBtn = document.getElementById("addMoneyBtn");
+const sendMoneyBtn = document.getElementById("sendMoneyBtn");
+
+addMoneyBtn.addEventListener("click", function () {
+    alert("VPay Add Money feature is coming soon.");
+});
+
+sendMoneyBtn.addEventListener("click", function () {
+    alert("VPay Send Money feature is coming soon.");
+});
